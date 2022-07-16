4c56203f8bdd4a474f08e9928a02ead2262dc21d
golurishav26 <golurishav26@gmail.com>
2022-07-16 23:06:42 +0200

    Some random java

---
+++ readme.java
@@ -0,0 +1,8 @@
+class readme{
+
+    public static void main(String[] args) {
+        double daysTolive = 18*365;
+        double moneyICanMake = daysTolive*50;
+        System.out.println("Money i will make :"+moneyICanMake);
+    }
+}
